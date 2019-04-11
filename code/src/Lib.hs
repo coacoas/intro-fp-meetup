8087bb50d96f93d8a4105c0da5a69d983cc7a15b
Bill Carlson <bill.carlson@banno.com>
2019-04-11 08:43:06 -0400

    Add data types to example code

--- code/src/Lib.hs
+++ code/src/Lib.hs
@@ -2,3 +2,24 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 
 module Lib where
+
+import Prelude hiding (not, even, odd)
+
+data Boolean = TRUE | FALSE
+
+not :: Bool -> Bool
+not True = False
+not False = True
+
+even :: Integer -> Bool
+even n = mod n 2 == 0
+-- even n = n `mod` 2 == 0
+
+divisibleBy :: Integer -> Integer -> Bool
+divisibleBy k n = n `mod` k == 0
+  
+even' = divisibleBy 2
+-- Function composition: (f ∘ g)(x) == f(g(x))
+-- f :: b -> c ; g :: a -> b ; (f ∘ g) :: a -> c
+odd   = not . even
+
